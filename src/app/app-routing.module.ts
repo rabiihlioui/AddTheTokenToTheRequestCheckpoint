320cf97eb016ef2e867bde70fa1e99d0152ee278
rabiihlioui <rabiihlioui@gmail.com>
2020-08-03 13:34:08 +0100

    Add The Token To The Request Checkpoint Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,36 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HeaderComponent } from './header/header.component';
+import { CvComponent } from './cv/cv.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { CvDetailsComponent } from './cv-details/cv-details.component';
+import { ErrorComponent } from './error/error.component';
+import { AddCvComponent } from './add-cv/add-cv.component';
+import { LoginComponent } from './login/login.component';
+import { LogoutComponent } from './logout/logout.component';
+
+const routes: Routes = [
+  { 
+    path: '',
+    component: HeaderComponent,
+    children: 
+    [ 
+      { path: '', component: LoginComponent },
+      { path: 'login', component: LoginComponent },
+      { path: 'logout', component: LogoutComponent },
+      { path: 'welcome', component: WelcomeComponent },
+      { path: 'cv',
+        component: CvComponent,
+        children: [
+          { path: ':id/cvDetails', component: CvDetailsComponent }
+        ]
+      },
+      { path: 'addCv', component: AddCvComponent }
+    ] 
+  },
+  { path: '**' , component:ErrorComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
